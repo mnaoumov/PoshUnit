154be230e790c26301030e47ab506ca344de924f
Michael Naumov <michael.naumov@readify.net>
2012-10-20 20:27:16 +1100

    Created Build-InstallPackage

---
+++ Build-InstallPackage.ps1
@@ -0,0 +1,27 @@
+#requires -version 2.0
+
+[CmdletBinding()]
+param
+(
+)
+
+$script:ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+$PSScriptRoot = $MyInvocation.MyCommand.Path | Split-Path
+
+$buildFolder = "$PSScriptRoot\_InstallPackage"
+
+if (Test-Path $buildFolder)
+{
+    Remove-Item $buildFolder -Recurse
+}
+
+New-Item $buildFolder -ItemType Directory
+New-Item "$buildFolder\packages\PoshUnit" -ItemType Directory
+New-Item "$buildFolder\Tests" -ItemType Directory
+
+Copy-Item -Path "$PSScriptRoot\packages\*" -Destination "$buildFolder\packages" -Recurse
+Copy-Item -Path $PSScriptRoot\NUnit.psm1 -Destination "$buildFolder\packages\PoshUnit"
+Copy-Item -Path $PSScriptRoot\PoshUnit.psm1 -Destination "$buildFolder\packages\PoshUnit"
+Copy-Item -Path $PSScriptRoot\Invoke-Tests.ps1 -Destination "$buildFolder"
+Copy-Item -Path $PSScriptRoot\Tests\TestFixture.Snippet.ps1 -Destination "$buildFolder\Tests"
